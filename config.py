9bbd1e298ffda6754e22986ae4cc5532dd8f714a
Shuto Tamaoka <shuto.tamaoka@gmail.com>
2024-09-12 11:03:43 +0900

    feat: define models

---
+++ config.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
+DATABASE = 'sqlite:///data.sqlite'
+
+engine = create_engine(
+    DATABASE,
+    # TrueだとSQL文が出力される
+    echo = False
+)
+
+session = Session(
+    autocommit = False,
+    autoflush = True,
+    bind = engine
+)
+
+base = declarative_base()
+#base.query = session.query_property()
