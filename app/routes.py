684243000503c0b7d30fa5d59ca7e8e210e7dcde
maomao <is0765rs@ed.ritsumei.ac.jp>
2024-09-15 01:18:34 +0900

    htmlで夜いただいた変数以外完成版

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,10 @@ def index():
     foods = get_all_food()
     return render_template("index.html", foods=foods)
 
-@app.route("/daily")
+@app.route("/daily")    
 def daily():
-#    food_list = get_food_by_meal_id(1)
-#    return render_template("daily.html", food_list=food_list)
-    history = get_history_by_user_id(1, is_recent=True)
-    return render_template("daily.html", history=history)
+    food_list = get_food_by_meal_id(1)
+    return render_template("daily.html", foods=foods)
+
+#    history = get_history_by_user_id(1, is_recent=False)
+#    return render_template("daily.html", history=history)
